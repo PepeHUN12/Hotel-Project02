90a80cc420da4ac12ace83753fa71dc8f6051b22
Peter Patyi <91049609+PepeHUN12@users.noreply.github.com>
2022-02-17 12:17:18 +0100

    Php szobák elkészítése ÉS room.php beillesztése
    UPDATE 02.17

---
+++ rooms/family.php
@@ -0,0 +1,18 @@
+<div class="single-room-area d-flex align-items-center mb-50 wow fadeInUp" data-wow-delay="700ms" style="visibility: visible; animation-delay: 700ms; animation-name: fadeInUp;">
+                        <!-- Room Thumbnail -->
+                        <div class="room-thumbnail">
+                            <img src="img/bg-img/46.jpg" alt="">
+                        </div>
+                        <!-- Room Content -->
+                        <div class="room-content">
+                            <h2>Room Vip King</h2>
+                            <h4>400$ <span>/ Day</span></h4>
+                            <div class="room-feature">
+                                <h6>Size: <span>30 ft</span></h6>
+                                <h6>Capacity: <span>Max persion 5</span></h6>
+                                <h6>Bed: <span>King beds</span></h6>
+                                <h6>Services: <span>Wifi, television ...</span></h6>
+                            </div>
+                            <a href="#" class="btn view-detail-btn">View Details <i class="fa fa-long-arrow-right" aria-hidden="true"></i></a>
+                        </div>
+                    </div>
