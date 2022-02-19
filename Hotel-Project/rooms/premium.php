74441e0326e4fb70be9b8fe0016ea4506e1885f7
Peter Patyi <91049609+PepeHUN12@users.noreply.github.com>
2022-02-19 17:41:00 +0100

    Update premium.php

--- Hotel-Project/rooms/premium.php
+++ Hotel-Project/rooms/premium.php
@@ -1,17 +1,17 @@
 <div class="single-room-area d-flex align-items-center mb-50 wow fadeInUp" data-wow-delay="500ms" style="visibility: visible; animation-delay: 500ms; animation-name: fadeInUp;">
                         <!-- Room Thumbnail -->
                         <div class="room-thumbnail">
-                            <img src="img/bg-img/45.jpg" alt="">
+                            <img src="img/bg-img/51.jpg" alt="">
                         </div>
                         <!-- Room Content -->
                         <div class="room-content">
-                            <h2>Premium King Room</h2>
-                            <h4>400$ <span>/ Day</span></h4>
+                            <h2>Premium Szoba</h2>
+                            <h4>550$ <span>/ Nap</span></h4>
                             <div class="room-feature">
-                                <h6>Size: <span>30 ft</span></h6>
-                                <h6>Capacity: <span>Max persion 5</span></h6>
-                                <h6>Bed: <span>King beds</span></h6>
-                                <h6>Services: <span>Wifi, television ...</span></h6>
+                                <h6>Size: <span>45 ft</span></h6>
+                                <h6>Férőhely: <span>2+2</span></h6>
+                                <h6>Ágy: <span>Franciaágy</span></h6>
+                                <h6>Szervíz: <span>Wifi, television, Szauna, Jacuzzi</span></h6>
                             </div>
                             <a href="#" class="btn view-detail-btn">Lefoglalás <i class="fa fa-long-arrow-right" aria-hidden="true"></i></a>
                         </div>
