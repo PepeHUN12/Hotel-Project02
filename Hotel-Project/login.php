85b11bd51d96720e696e7e57f91c8b96d6518fe0
Peter Patyi <peti.patyi2000@gmail.com>
2022-02-28 22:52:31 +0100

    Login.php responsive
    -mostmár responsive

--- Hotel-Project/login.php
+++ Hotel-Project/login.php
@@ -75,21 +75,19 @@
                     <!-- Form -->
                     <div class="roberto-contact-form">
                         <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
-                            <div class="row" style="padding-left:34%" >
+                            <div class="row">
                                 <div class="col-12 col-lg-6 wow fadeInUp" data-wow-delay="100ms" style="visibility: visible; animation-delay: 100ms; animation-name: fadeInUp;">
-                                    <input style="border: solid black 1px" type="email" name="getemail" class="form-control mb-30" placeholder="Email-Cím">
-                                </div> 
-                            </div>
-                            <div class="row" style="padding-left:34%">
-                                <div class="col-12 col-lg-6 wow fadeInUp" data-wow-delay="100ms" style="visibility: visible; animation-delay: 100ms; animation-name: fadeInUp;">
-                                    <input style="border: solid black 1px" type="password" name="getpassword" class="form-control mb-30" placeholder="Jelszó">
+                                    <input type="email" name="firstname" class="form-control mb-30" placeholder="Email">
                                 </div>
+                                <div class="col-12 col-lg-6 wow fadeInUp" data-wow-delay="100ms" style="visibility: visible; animation-delay: 100ms; animation-name: fadeInUp;">
+                                    <input type="password" name="lastname" class="form-control mb-30" placeholder="Jelszó">
+                                </div>
+                                
                                 <div class="col-12 text-center wow fadeInUp" data-wow-delay="100ms" style="visibility: visible; animation-delay: 100ms; animation-name: fadeInUp;">
+                                    <button type="submit" class="btn roberto-btn mt-15">Bejelenzkezés</button>
+                                    <a href="registration.php" class="btn roberto-btn mt-15"> Regisztráció </a>
                                 </div>
-                            </div>
-                            <div class="section-heading text-center wow fadeInUp" data-wow-delay="100ms" style="visibility: visible; animation-delay: 100ms; animation-name: fadeInUp;">
-                            <button type="submit" class="btn roberto-btn mt-15">Bejelentkezés</button>    
-                            <a href="registration.php" class="btn roberto-btn mt-15"> Regisztráció </a>
+                                
                             </div>
                         </form>
                     </div>
