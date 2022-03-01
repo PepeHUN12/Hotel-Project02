94701468d2c2ae0365e04876c1e82650307adeb1
Peter Patyi <91049609+PepeHUN12@users.noreply.github.com>
2022-03-01 20:03:47 +0100

    Update header.php

--- Hotel-Project/header.php
+++ Hotel-Project/header.php
@@ -19,7 +19,7 @@
                         <div class="top-header-content">
                             <a href="#"><i class="icon_phone"></i> <span>(+36) 30-789-1230</span></a>
                             <a href="#"><i class="icon_mail"></i> <span>schola@scholaeu.hu</span></a>
-                            <a name="log/reg" href="login.php">Bejelentkezés</a>
+                            
 
                         </div>
                     </div>
@@ -29,9 +29,9 @@
                             <!-- Top Social Area -->
                             <div class="top-social-area ml-auto">
                                 <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
-                                <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
-                                <a href="#"><i class="fa fa-tripadvisor" aria-hidden="true"></i></a>
+                                <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i>
                                 <a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                                <a style="color:red" name="log/reg" href="login.php">Bejelentkezés</a>
                             </div>
                         </div>
                     </div>
